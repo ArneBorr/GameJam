1e9f9b975ab4a0b291a3b757157c958c461e4129
Arne <arneborr@gmail.com>
2022-03-10 21:13:03 +0100

    [MOD] Add ghost files Booo

---
+++ GameJam03_2022/Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+
+    private float x = 4.6f;
+    private float z = 4.6f;
+    private float y = 0.3f;
+
+    private void Start()
+    {
+        Vector3 position = new Vector3(Random.Range(-x, x), y, Random.Range(-z, z));
+        PhotonNetwork.Instantiate(_playerPrefab.name, position, Quaternion.identity);
+    }
+
+
+}
