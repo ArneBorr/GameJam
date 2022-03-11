f3094c14c73106c816815149ef288121ab1f5f3c
Arne <arneborr@gmail.com>
2022-03-11 23:19:12 +0100

    [MOD] Geen idee help

--- GameJam03_2022/Assets/Scripts/Huisstofmijt/Huisstofmijt.cs
+++ GameJam03_2022/Assets/Scripts/Huisstofmijt/Huisstofmijt.cs
@@ -1,9 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Huisstofmijt : MonoBehaviour
 {
+    private Transform _child;
+    private SpriteRenderer _visuals;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _visuals = GetComponentInChildren<SpriteRenderer>();
+        _child = GetComponentInChildren<Transform>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _child.rotation = Quaternion.identity;
+        _visuals.flipX = ;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
