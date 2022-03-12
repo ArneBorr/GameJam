d33022deb91db87dfb7c78a8c5e1abdafece3506
Arne <arneborr@gmail.com>
2022-03-12 13:56:44 +0100

    ?

--- GameJam03_2022/Assets/Scripts/PlayerName.cs
+++ GameJam03_2022/Assets/Scripts/PlayerName.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 
 public class PlayerName : MonoBehaviour
 {
+    [SerializeField] private GameObject _uiPrefab;
+
+    GameObject _listingElement;
+    List<TextMeshProUGUI> _textScores = new List<TextMeshProUGUI>();
     TextMeshPro _playerName = null;
     PhotonView _view = null;
 
@@ -12,6 +16,7 @@ public class PlayerName : MonoBehaviour
     {
         _playerName = GetComponentInChildren<TextMeshPro>();
         _view = GetComponent<PhotonView>();
+        _listingElement = GameObject.Find("ScoreListing");
 
         if (_view.IsMine)
         {
@@ -20,6 +25,57 @@ public class PlayerName : MonoBehaviour
         else
         {
             _playerName.text = _view.Owner.NickName;
+        }
+    }
+
+    private void Update()
+    {
+        if (_view.IsMine) 
+        {
+            UpdateScoreTexts();
+            UpdateScores();
+        }
+    }
+
+    private void UpdateScoreTexts()
+    {
+        int playerCount = PhotonNetwork.PlayerList.Length - 1;
+
+        while (_textScores.Count < playerCount)
+        {
+            GameObject ui = Instantiate(_uiPrefab);
+            ui.transform.parent = _listingElement.transform;
+            _textScores.Add(ui.GetComponentInChildren<TextMeshProUGUI>());
+        }
+
+        while (_textScores.Count > playerCount)
+        {
+            int i = _textScores.Count - 1;
+            GameObject obj = _textScores[i].gameObject;
+            _textScores.RemoveAt(i);
+            Destroy(obj.transform.parent.gameObject);
+        }
+    }
+
+    private void UpdateScores() 
+    {
+        int i = 0;
+        foreach (Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                //int score = player.TagObject;
+                if(PhotonNetwork.LocalPlayer.TagObject == null) 
+                {
+                    _textScores[i].text = "000";
+                }
+                else 
+                {
+                    _textScores[i].text = (player.TagObject as GameObject).GetComponent<Player>().TotalScore.ToString();
+                }
+
+                i++;
+            }
         }
     }
 }
